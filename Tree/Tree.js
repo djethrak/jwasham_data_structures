deae52b98cf50a3f02882fc9b2c860a87c713c0f
djethrak <enarebebenatthan@gmail.com>
2023-09-23 01:41:06 -0700

    Update Tree.js

--- Tree/Tree.js
+++ Tree/Tree.js
@@ -158,6 +158,7 @@ class Tree {
                     if (currentTreeNode.left == null ) {
                         // insert
                         loopEnabled = false
+                        newTreeNode.parent = currentTreeNode
                         currentTreeNode.left = newTreeNode
                         this.length++
                     }else{
@@ -168,6 +169,7 @@ class Tree {
                     if (currentTreeNode.right == null ) {
                         // insert
                         loopEnabled = false
+                        newTreeNode.parent = currentTreeNode
                         currentTreeNode.right = newTreeNode
                         this.length++
                     }else{
@@ -180,11 +182,92 @@ class Tree {
 
     }
 
-    travasalInOrder(){
+    update(value,atValue){
+        var isNotFound = true
+        var tree = this.tree
+
+        while(isNotFound){
+            if (tree.value == atValue) {
+                tree.value = value
+                isNotFound = false
+            }else{
+                if (tree.value < atValue) {
+                    if (tree.right != null) {
+                        tree = tree.right 
+                    }
+                    else{
+                        isNotFound = false
+                        console.log("Value not found!")
+                    }
+                    
+                }else{
+                    if (tree.left != null) {
+                        tree = tree.left 
+                    }
+                    else{
+                        isNotFound = false
+                        console.log("Value not found!")
+                    }
+                }
+            }
+        }
+    }
+
+    delete(value){
+        var isNotFound = true
+        var tree = this.tree
+
+        while(isNotFound){
+            if (tree.value == value) {
+                if (this.isLeaf(tree)) {
+                    // Let's delete leaf 
+                    if (tree.parent != null) {
+                        if (tree.parent.value < value) {
+                            tree.parent.right = null 
+                        }
+                        else{
+                            tree.parent.left = null 
+                        }
+                        this.length--
+                    }else{
+                        // This means we have only one parent
+                        this.tree = {}
+                        this.length = 0
+                    }
+                }
+                isNotFound = false
+            }else{
+                if (tree.value < value) {
+                    if (tree.right != null) {
+                        tree = tree.right 
+                    }
+                    else{
+                        isNotFound = false
+                        console.log("Value not found!")
+                    }
+                    
+                }
+                else{
+                    if (tree.left != null) {
+                        tree = tree.left 
+                    }
+                    else{
+                        isNotFound = false
+                        console.log("Value not found!")
+                    }
+                }
+            }
+        }
+    }
+
+    isLeaf(treeNode){
+        return (treeNode.left == null && treeNode.right == null)
+    }
+
+    travasalLevelOrder(){
         if (this.length == 0) {
             console.log("No TreeNode in tree!")
         }else{
-            var loopEnabled = true
             const queue = new QueueLinkedList()
             queue.enqueque(this.tree)
 
@@ -203,14 +286,30 @@ class Tree {
         }
 
     }
+
 }
 
 const tree =  new Tree()
 
 tree.add(10)
+tree.add(7)
 tree.add(9)
+tree.add(5)
 tree.add(90)
 
-tree.travasalInOrder()
+tree.update(20,90)
+
+tree.travasalLevelOrder()
+console.log("\n\n")
+
+// tree.delete(20)
+// tree.delete(7)
+// tree.delete(5)
+// tree.delete(9)
+// tree.delete(7)
+// tree.delete(10)
+
+
+tree.travasalLevelOrder()
 
 // console.log(tree)
