aa4ea486cb13bc66a7b0bfd808b3abf004dbc4d2
Abraham <enarebebenatthan@gmail.com>
2023-10-25 05:23:55 -0700

    Create Heap

---
+++ Javascript/Heap/MinHeap.js
@@ -0,0 +1,12 @@
+/*
+This code was created by Enarebebe Abraham Ebimawe
+mail: enarebebenatthan@gmail.com
+*/
+
+class MinHeap{
+    constructor() {
+        this.array = []
+    }
+
+    insert(value){}
+}
