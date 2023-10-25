68127299e0afac064abdb035caf74775ab107c2e
Abraham <enarebebenatthan@gmail.com>
2023-10-25 05:57:51 -0700

    important checks and functions in min heap

--- Javascript/Heap/MinHeap.js
+++ Javascript/Heap/MinHeap.js
@@ -8,7 +8,13 @@ class MinHeap{
         this.array = []
     }
 
-    insert(value){}
+    insert(value){
+        if (this.array.length == 0) {
+            this.array.push(value)
+        }else{
+            console.log("object");
+        }
+    }
 
     remove(index){}
 
@@ -17,7 +23,41 @@ class MinHeap{
     heapify(array){}
 
     heapSort(){}
+
+    // private classes
+
+     getTheLastParent(){
+        return this.array.length/2
+    }
+    isNodeFull(index){
+        return this.array[(index*2)+1] != undefined
+    }
+    isLeaf(index){
+        return this.array[(index*2)] == undefined
+    }
+    isMinHeap(index){
+        if (this.isLeaf(index)) {
+            return true
+        }else{
+            var left = this.array[(index*2)+1]
+            var right = this.array[(index*2)]
+            var parent =this.array[index]
+            if (right  < parent) {
+                return false
+            } else if (left != undefined && left  < parent) {
+                return false
+            } else{
+                return true
+            }
+        }
+    }
+    swapNode(index1, index2){
+        var temp = this.array[index1]
+        this.array[index1] = this.array[index2]
+        this.array[index2] = temp
+    }
 }
+
 
 module.exports = {
     MinHeap
