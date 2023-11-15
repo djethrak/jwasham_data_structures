037fb8a81c98c9dd90235907ef6f0e062cc8d435
Abraham <enarebebenatthan@gmail.com>
2023-11-15 06:02:09 -0800

    Dijkstra 3 pointer algorithm

--- Javascript/Sorting/QuickSort/Dijkstra3pointer.js
+++ Javascript/Sorting/QuickSort/Dijkstra3pointer.js
@@ -1,12 +1,35 @@
 class Dijkstra3pointer{
+    /*
+    This algorithm is good for duplicates
+    */
     constructor(array) {
         this.array = array
     }
     
     make(){
-        pointer = 0
-        low = 1
-        high = this.array.length - 1
+        /*
+        Pointer is the pivot that set the upper and lower bound
+        low does move if value is low and vice versa
+        */
+        var pointer = 0
+        var low = 1
+        var high = this.array.length - 1
+
+        while (low < high) {
+            if(this.array[low] > this.array[pointer] && this.array[high] <= this.array[pointer]){
+                this.swap(low,high)
+                // move pointer to the back of new low 
+                this.swap(low,pointer)
+                pointer = low
+                low++
+                high--
+            }
+            
+            if (this.array[high] > this.array[pointer]) {
+                high--
+            }
+        }
+        console.log(this.array);
 
     }
 
@@ -16,3 +39,7 @@ class Dijkstra3pointer{
        this.array[j] = temp
     }
 }
+
+// const dijkstra = new Dijkstra3pointer([5,86,69,73,11,17,1,74,34,3])
+const dijkstra = new Dijkstra3pointer([1,4,8,6,1,1,10])
+dijkstra.make()
