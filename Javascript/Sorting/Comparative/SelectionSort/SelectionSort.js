566ae68bef40e607fed6631235a335cd21c88993
Abraham <enarebebenatthan@gmail.com>
2023-11-24 00:12:47 -0800

    SelectionSort

---
+++ Javascript/Sorting/Comparative/SelectionSort/SelectionSort.js
@@ -0,0 +1,37 @@
+class SelectionSort {
+    constructor(array) {
+        this.array = array
+    }
+
+    sort(){
+
+        for (let i = 0; i < this.array.length; i++) {
+            this.swap(i,this.findMinIndex(i))
+            
+        }
+        console.log(this.array);
+    }
+    findMinIndex(from){
+        // Look for min from start to end then replace at appropriate
+        var min = this.array[from]
+        var minIndex = from
+
+
+        for (let i = from; i < this.array.length; i++) {
+            if (this.array[i]< min) {
+                min = this.array[i]
+                minIndex = i
+            }
+            
+        }
+        return minIndex
+    }
+    swap(left, right){
+        var temp = this.array[left]
+        this.array[left] = this.array[right]
+        this.array[right] = temp
+    }
+}
+
+const selectionSort = new SelectionSort([9,0,2,0,4])
+selectionSort.sort()
